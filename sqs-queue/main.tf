5d5e6198a09d24c0c256ee6831e87db441cf9879
Kang-Kyu Lee <kangkyu1111@gmail.com>
2022-11-29 22:17:50 -0800

    Initial commit - sqs queue

---
+++ sqs-queue/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_sqs_queue" "test_queue" {
+  name                      = "${var.sqs_prefix}-${var.env_name}"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  # sqs_managed_sse_enabled = true
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.test_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Env = var.env_name
+  }
+
+  depends_on = [
+    aws_sqs_queue.test_queue_deadletter
+  ]
+}
+
+resource "aws_sqs_queue" "test_queue_deadletter" {
+  name = "deadletter-queue-${var.sqs_prefix}-${var.env_name}"
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.test_queue.arn]
+  })
+}
---
+++ sqs-queue/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "us-west-2"
+  type = string
+}
+
+variable "sqs_prefix" {
+  type = string
+}
+
+variable "env_name" {
+  type = string
+}
